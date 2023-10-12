2ed8e7830705871445ab9413ed20b077017871d7
Caleb Caliber <cjnorman@my.waketech.edu>
2023-10-11 23:10:59 -0400

    New Unity Project with Lighting and Crud
    Lighting, Jaydin's stuff integrated, and also a track texture woooo.

---
+++ NewMinecartGame/Assets/Scripts/PlayerAimWeapon.cs
@@ -0,0 +1,87 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAimWeapon : MonoBehaviour
+{
+    public GameObject gunAnimation;
+
+    public event EventHandler<onShootEventArgs> onShoot;
+    public class onShootEventArgs : EventArgs
+    {
+        public Vector3 gunEndPointPosition;
+        public Vector3 shootPosition;
+        public Vector3 BulletPosition;
+    }
+
+    private Transform aimTransform;
+    private Transform aimGunEndPointTransform;
+    private Transform aimBulletPositionTransform;
+
+    
+
+    private void Awake()
+    {
+        aimTransform = transform.Find("Aim").transform.GetChild(0);
+        aimGunEndPointTransform = aimTransform.Find("GunEndPointPosition");
+
+
+    }
+
+    private void Update()
+    {
+        HandleAiming();
+        handleShooting();
+    }
+
+
+    private void HandleAiming()
+    {
+        Vector3 mousePosition = GetMousePosition();
+
+        Vector2 aimDirection = (mousePosition - transform.position).normalized;
+        float angle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg;
+        aimTransform.eulerAngles = new Vector3(0, 0, angle);
+    }
+
+    private void handleShooting()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePosition = GetMousePosition();
+            onShoot?.Invoke(this, new onShootEventArgs
+            {
+                gunEndPointPosition = aimGunEndPointTransform.position,
+                shootPosition = mousePosition,
+                 BulletPosition = aimBulletPositionTransform.position,
+            });
+            Debug.Log(mousePosition);
+
+            //makes muzzle flare
+            gunAnimation.gameObject.SetActive(true);
+            gunAnimation.GetComponent<Animator>().Play("MuzzleFlare");
+        }
+    }
+
+
+    public static Vector3 GetMousePosition()
+    {
+        Vector3 vec = GetMousePositionWithZ(Input.mousePosition, Camera.main);
+        vec.z = 0f;
+        return vec;
+    }
+    public static Vector3 GetMousePositionWithZ()
+    {
+        return GetMousePositionWithZ(Input.mousePosition, Camera.main);
+    }
+    public static Vector3 GetMousePositionWithZ(Camera worldCamera)
+    {
+        return GetMousePositionWithZ(Input.mousePosition, worldCamera);
+    }
+    public static Vector3 GetMousePositionWithZ(Vector3 screenPosition, Camera worldCamera)
+    {
+        Vector3 worldPosition = worldCamera.ScreenToWorldPoint(screenPosition);
+        return worldPosition;
+    }
+}
