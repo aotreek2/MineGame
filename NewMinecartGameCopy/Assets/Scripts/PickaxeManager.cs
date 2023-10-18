453c45f1cd1502b95bfd1c73507e8c06bd923be5
Caleb Caliber <cjnorman@my.waketech.edu>
2023-10-17 20:53:31 -0400

    Not Meant to upload

---
+++ NewMinecartGameCopy/Assets/Scripts/PickaxeManager.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PickaxeManager : MonoBehaviour
+{
+
+    public bool inRange = false;
+    public GameObject currentOre;
+    public OreManager oreManager;
+
+    public TextMeshProUGUI oreStats;
+    public int gold = 0;
+    public int diamond = 0;
+    public int invine = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            transform.GetComponent<Animator>().Play("PickaxeSwing");
+            if (inRange && oreManager.oreHealth > 0)
+            {
+                if (currentOre.tag == "goldOre")
+                {
+                    gold++;
+                }
+                else if (currentOre.tag == "diamondOre")
+                {
+                    diamond++;
+                }
+                else if (currentOre.tag == "invineOre")
+                {
+                    invine++;
+                }
+                else
+                {
+                    Debug.Log("Something went wrong. Or you mined rock.");
+                }
+
+                oreManager.oreHealth--;
+                
+
+                UpdateOres();
+            }
+
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "goldOre" || collision.tag == "diamondOre" || collision.tag == "invineOre" || collision.tag == "rock")
+        {
+            currentOre = collision.gameObject;
+            oreManager = collision.GetComponent<OreManager>();
+            inRange = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "goldOre" || collision.tag == "diamondOre" || collision.tag == "invineOre" || collision.tag == "rock")
+        {
+            inRange = false;
+        }
+    }
+
+    public void UpdateOres()
+    {
+        oreStats.text = $"Gold: {gold} / Diamond: {diamond} / Invine: {invine}";
+    }
+
+    public void Sparks()
+    {
+        if (inRange)
+        {
+            currentOre.transform.parent.GetChild(1).transform.GetComponent<ParticleSystem>().Play();
+            //currentOre.transform.GetChild(0).transform.GetComponent<ParticleSystem>().Play();
+
+            if (oreManager.oreHealth <= 0)
+            {
+                currentOre.SetActive(false);
+            }
+        }
+    }
+}
---
+++ NewMinecartGameCopy/Assets/Scripts/Zombie.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zombie : MonoBehaviour
+{
+    public Transform player;
+    private float distance;
+    public float speed;
+    public float distanceBetween;
+    public Animator zombie;
+    private Rigidbody2D rb;
+   
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distance = Vector2.Distance(transform.position, player.transform.position);   // getting the distance between the player and the zombie
+        Vector2 direction = player.transform.position - transform.position;
+        direction.Normalize();
+        float angle = Mathf.Atan(direction.x);    // getting and calculating the correct direction for the zombie to face
+
+        if (distance < distanceBetween)
+        {
+            zombie.Play("zombie move");
+
+            Vector2 target = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime); //sets up when the zombie chases the player
+            rb.velocity = direction * speed;
+            transform.rotation = Quaternion.Euler(Vector3.forward * angle);
+            //zombie.SetTrigger("Vision");
+        }
+    }
+}
+
